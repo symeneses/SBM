7e98e8775dc5d20d33d275b6018a3d1b128c3b26
symeneses <sandrayojana@gmail.com>
2023-02-27 11:34:32 +0100

    :bug: add stop

--- src/utils.py
+++ src/utils.py
@@ -6,12 +6,13 @@ import tracemalloc
 def monitor(func):
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
-        tracemalloc.start()
         tic = time.perf_counter()
+        tracemalloc.start()
         value = func(*args, **kwargs)
-        toc = time.perf_counter()
         current_memory, peak_memory = tracemalloc.get_traced_memory()
+        toc = time.perf_counter()
         elapsed_time = toc - tic
+        tracemalloc.stop()
         return value, {"elapsed_time": elapsed_time,
                        "current_memory": current_memory,
                        "peak_memory": peak_memory}
